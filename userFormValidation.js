a9169c64575b585e603e610b6868eced0fe8dec6
FESA ONE <133826879+fesaone@users.noreply.github.com>
2023-05-19 13:54:21 +0700

    Update userFormValidation.js

--- userFormValidation.js
+++ userFormValidation.js
@@ -1,11 +1,11 @@
 window.onload = function() {
-  fetch('database/form.html')
+  fetch('https://cdn.jsdelivr.net/gh/fesaone/simplekredit/form.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('form').innerHTML = data;
       // Load your JavaScript file after the HTML is loaded
       var script = document.createElement('script');
-      script.src = "database/countData.js"; 
+      script.src = "https://cdn.jsdelivr.net/gh/fesaone/simplekredit/script.js"; 
       document.body.appendChild(script);
     });
 };
